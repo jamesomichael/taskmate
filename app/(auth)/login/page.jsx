20568b96a5c3f8b7102ca6cc0ffa81049104c228
James Michael <jomichael1994@gmail.com>
2025-02-10 18:47:30 +0000

    add redirect for authenticated users on login and signup pages

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { redirect } from 'next/navigation';
+
+import { createClient } from '@/utils/supabase/server';
 
 import { login } from './actions';
 import AccountForm from '@/components/AccountForm';
 
-const LoginPage = () => {
+const LoginPage = async () => {
+	const supabase = await createClient();
+	const { data, error } = await supabase.auth.getUser();
+
+	if (!error && data.user) {
+		redirect('/');
+	}
+
 	return (
 		<div className="p-6 h-full">
 			<AccountForm formAction={login} type="login" />
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { redirect } from 'next/navigation';
+
+import { createClient } from '@/utils/supabase/server';
 
 import { createAccount } from './actions';
 import AccountForm from '@/components/AccountForm';
 
-const SignUpPage = () => {
+const SignUpPage = async () => {
+	const supabase = await createClient();
+	const { data, error } = await supabase.auth.getUser();
+
+	if (!error && data.user) {
+		redirect('/');
+	}
+
 	return (
 		<div className="p-6 h-full">
 			<AccountForm formAction={createAccount} type="signup" />
