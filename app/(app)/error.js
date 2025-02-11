8f75e488571ed8da996debc6836ee8f0a0ce0f46
James Michael <jomichael1994@gmail.com>
2025-02-11 23:36:26 +0000

    add basic error page

---
+++ app/(app)/error.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const ErrorPage = () => {
+	return (
+		<div className="select-none flex justify-center items-center h-full">
+			<span className="font-heading">Sorry, something went wrong.</span>
+		</div>
+	);
+};
+
+export default ErrorPage;
