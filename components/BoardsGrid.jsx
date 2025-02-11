d356a37fec191763a708d7688b571e9f21238f56
James Michael <jomichael1994@gmail.com>
2025-02-11 20:39:54 +0000

    add gap to BoardsGrid, add padding to Modal

--- components/BoardsGrid.jsx
+++ components/BoardsGrid.jsx
@@ -53,7 +53,7 @@ const BoardsGrid = ({ boards: initialBoards, userId }) => {
 	];
 
 	return (
-		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6">
+		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 sm:gap-4">
 			{boards.map((board) => {
 				return (
 					<Link
