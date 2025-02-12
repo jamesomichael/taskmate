970bac98ff9def7d854a8a9477be8d66dc9ae5cc
James Michael <jomichael1994@gmail.com>
2025-02-12 15:44:15 +0000

    update layout structure

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -11,12 +11,14 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body
-				className={'antialiased min-h-screen h-screen w-full bg-white'}
+				className={
+					'antialiased min-h-screen h-screen w-full grid grid-rows-[auto,1fr] bg-white'
+				}
 			>
 				<div className="select-none h-12 border-b-[1px] border-gray-300">
 					<Navbar />
 				</div>
-				{children}
+				<div className="overflow-auto">{children}</div>
 			</body>
 		</html>
 	);
