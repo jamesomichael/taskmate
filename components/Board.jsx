6677a73beea4642e9b9310ad0f6b40db4e7ccadd
James Michael <jomichael1994@gmail.com>
2025-02-19 11:49:55 +0000

    add star toggle to board header

--- components/Board.jsx
+++ components/Board.jsx
@@ -5,6 +5,7 @@ import Lists from './Lists';
 import Loader from './Loader';
 
 import useBoardStore from '@/stores/boardStore';
+import { FaRegStar, FaStar } from 'react-icons/fa6';
 
 const Board = ({ id }) => {
 	const { isLoading, board, getBoard } = useBoardStore();
@@ -19,10 +20,17 @@ const Board = ({ id }) => {
 		<div
 			className={`grid grid-rows-[auto,1fr] bg-gradient-to-br ${board.background} h-full overflow-hidden w-full`}
 		>
-			<div className="h-14 bg-black bg-opacity-45 flex items-center px-4">
+			<div className="h-14 bg-black bg-opacity-45 flex gap-4 items-center px-4 py-3">
 				<span className="font-copy font-semibold text-white text-lg">
 					{board.name}
 				</span>
+				<div className="h-full aspect-square rounded flex justify-center items-center text-yellow-400 hover:bg-white hover:bg-opacity-25 hover:cursor-pointer text-sm">
+					{board.is_starred ? (
+						<FaStar />
+					) : (
+						<FaRegStar className="text-white" />
+					)}
+				</div>
 			</div>
 			<Lists />
 		</div>
