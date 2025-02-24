063af1b1648b1e22848279c466cdb7fcbdbf6d46
James Michael <jomichael1994@gmail.com>
2025-02-24 20:41:38 +0000

    adjust modal positioning, update close icon

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { IoClose } from 'react-icons/io5';
+
 const Modal = ({ isOpen, onClose, children, width = 'max-w-md' }) => {
 	if (!isOpen) {
 		return null;
@@ -15,16 +17,16 @@ const Modal = ({ isOpen, onClose, children, width = 'max-w-md' }) => {
 		<div
 			id="backdrop"
 			onClick={handleBackdropClick}
-			className="z-50 fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 p-4"
+			className="z-50 fixed inset-0 flex items-start justify-center bg-black bg-opacity-75 px-4 py-12"
 		>
 			<div
 				className={`relative bg-white p-6 rounded shadow-lg w-full ${width}`}
 			>
 				<button
-					className="absolute top-2 right-3 w-8 rounded aspect-square text-gray-500 hover:bg-gray-300 font-black font-copy"
+					className="flex justify-center items-center absolute top-2 right-3 w-8 rounded aspect-square text-gray-500 hover:bg-gray-300 font-black font-copy"
 					onClick={onClose}
 				>
-					✕
+					<IoClose size={25} />
 				</button>
 				{children}
 			</div>
