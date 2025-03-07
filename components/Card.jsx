8e27b16f01a69fb43adcb271f76583d3d27b56a3
James Michael <jomichael1994@gmail.com>
2025-03-07 23:07:02 +0000

    add context menu to Card component, allow deletions

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { FaRegCircle, FaCircleCheck } from 'react-icons/fa6';
 
+import ContextMenu from './ContextMenu';
+
 import useBoardStore from '@/stores/boardStore';
 
 const Card = ({ card, isBeingDragged }) => {
-	const { setActiveCard, updateCard, draggedCard } = useBoardStore();
+	const { setActiveCard, updateCard, deleteCard, draggedCard } =
+		useBoardStore();
 
 	const toggleCompletionStatus = async (e) => {
 		e.stopPropagation();
@@ -17,43 +20,59 @@ const Card = ({ card, isBeingDragged }) => {
 		);
 	};
 
+	const handleCardDeletion = async () => {
+		await deleteCard(card.id, card.board_id, card.list_id);
+	};
+
 	return (
-		<div
-			onClick={() => setActiveCard(card)}
-			className={`group shadow-xl ${
-				draggedCard?.id === card.id && !isBeingDragged
-					? 'bg-black opacity-25'
-					: 'bg-white'
-			} h-fit rounded-md outline outline-[1px] outline-gray-300 hover:outline-2 hover:outline-blue-600 hover:cursor-pointer ${
-				isBeingDragged ? 'rotate-3 shadow-lg' : ''
-			}`}
-		>
-			{card.cover && (
-				<div className={`${card.cover} h-8 w-full rounded-t`}></div>
-			)}
-			<div className="grid grid-rows-[auto,auto] grid-cols-[auto,1fr] min-h-10 gap-x-1.5 p-2">
-				<div className="py-1 flex justify-center items-center">
-					{card.is_complete ? (
-						<FaCircleCheck
-							size={15}
-							onClick={toggleCompletionStatus}
-							title="Mark incomplete"
-							className="text-green-600"
-						/>
-					) : (
-						<FaRegCircle
-							size={15}
-							onClick={toggleCompletionStatus}
-							title="Mark complete"
-							className="hidden group-hover:flex"
-						/>
+		<ContextMenu
+			trigger={
+				<div
+					onClick={() => setActiveCard(card)}
+					className={`group shadow-xl ${
+						draggedCard?.id === card.id && !isBeingDragged
+							? 'bg-black opacity-25'
+							: 'bg-white'
+					} h-fit rounded-md outline outline-[1px] outline-gray-300 hover:outline-2 hover:outline-blue-600 hover:cursor-pointer ${
+						isBeingDragged ? 'rotate-3 shadow-lg' : ''
+					}`}
+				>
+					{card.cover && (
+						<div
+							className={`${card.cover} h-8 w-full rounded-t`}
+						></div>
 					)}
+					<div className="grid grid-rows-[auto,auto] grid-cols-[auto,1fr] min-h-10 gap-x-1.5 p-2">
+						<div className="py-1 flex justify-center items-center">
+							{card.is_complete ? (
+								<FaCircleCheck
+									size={15}
+									onClick={toggleCompletionStatus}
+									title="Mark incomplete"
+									className="text-green-600"
+								/>
+							) : (
+								<FaRegCircle
+									size={15}
+									onClick={toggleCompletionStatus}
+									title="Mark complete"
+									className="hidden group-hover:flex"
+								/>
+							)}
+						</div>
+						<span className="font-copy row-span-2 text-sm self-center line-clamp-4">
+							{card.title}
+						</span>
+					</div>
 				</div>
-				<span className="font-copy row-span-2 text-sm self-center line-clamp-4">
-					{card.title}
-				</span>
-			</div>
-		</div>
+			}
+			actions={[
+				{
+					label: 'Delete Card',
+					onClick: handleCardDeletion,
+				},
+			]}
+		/>
 	);
 };
 
