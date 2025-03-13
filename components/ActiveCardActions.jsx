438dcd51f981473df3c31b6e8dadf91893f14fe6
James Michael <jomichael1994@gmail.com>
2025-03-13 15:36:09 +0000

    add confirmation modal for active card deletions

--- components/ActiveCardActions.jsx
+++ components/ActiveCardActions.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Popover from '@/components/Popover';
 import CoverPopover from './CoverPopover';
+import ConfirmModal from './ConfirmModal';
+import ActiveCardAction from './ActiveCardAction';
 
 import { FaRegTrashCan, FaRegWindowMaximize } from 'react-icons/fa6';
 
 import useBoardStore from '@/stores/boardStore';
-import ActiveCardAction from './ActiveCardAction';
 
 const ActiveCardActions = () => {
 	const { activeCard, setActiveCard, deleteCard } = useBoardStore();
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const handleCardDeletion = async () => {
 		await deleteCard(
@@ -21,27 +23,42 @@ const ActiveCardActions = () => {
 	};
 
 	return (
-		<div className="flex flex-col gap-2">
-			<span className="text-xs font-copy font-semibold">Actions</span>
-			<div className="relative">
-				<Popover
-					trigger={
-						<ActiveCardAction
-							icon={FaRegWindowMaximize}
-							label="Cover"
-						/>
-					}
-				>
-					<CoverPopover />
-				</Popover>
+		<>
+			<div className="flex flex-col gap-2">
+				<span className="text-xs font-copy font-semibold">Actions</span>
+				<div className="relative">
+					<Popover
+						trigger={
+							<ActiveCardAction
+								icon={FaRegWindowMaximize}
+								label="Cover"
+							/>
+						}
+					>
+						<CoverPopover />
+					</Popover>
+				</div>
+				<ActiveCardAction
+					onClick={() => setIsModalOpen(true)}
+					icon={FaRegTrashCan}
+					label="Delete"
+					className="hover:bg-red-700 bg-red-600 text-white"
+				/>
 			</div>
-			<ActiveCardAction
-				onClick={handleCardDeletion}
-				icon={FaRegTrashCan}
-				label="Delete"
-				className="hover:bg-red-700 bg-red-600 text-white"
-			/>
-		</div>
+
+			<ConfirmModal
+				isOpen={isModalOpen}
+				onCancel={() => setIsModalOpen(false)}
+				onClose={() => setIsModalOpen(false)}
+				onConfirm={handleCardDeletion}
+				confirmationButtonText="Delete Card"
+			>
+				<span className="font-copy">
+					This card will be permanently deleted. This action cannot be
+					undone.
+				</span>
+			</ConfirmModal>
+		</>
 	);
 };
 
