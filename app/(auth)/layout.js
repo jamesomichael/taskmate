0ede0748709b3b6fce2f52b4aa0cc3b5f8d6ddf3
James Michael <jomichael1994@gmail.com>
2025-03-18 15:30:36 +0000

    add apple icon

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -7,6 +7,10 @@ export const metadata = {
 		icon: '/favicon-96x96.png',
 		apple: '/apple-icon-180x180.png',
 		android: '/android-icon-192x192.png',
+		other: {
+			rel: 'apple-touch-icon-precomposed',
+			url: '/apple-icon-precomposed.png',
+		},
 	},
 };
 
