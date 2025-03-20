5fa7ef25c4783b37660fbce4ab35c25de6f4288e
James Michael <jomichael1994@gmail.com>
2025-03-20 18:14:07 +0000

    allow additional input props

--- components/Input.jsx
+++ components/Input.jsx
@@ -6,8 +6,8 @@ const Input = ({
 	value,
 	placeholder = '',
 	disabled = false,
-	maxLength,
 	className,
+	...props
 }) => {
 	return (
 		<input
@@ -16,12 +16,12 @@ const Input = ({
 			placeholder={placeholder}
 			value={value}
 			onChange={onChange}
-			maxLength={maxLength}
 			className={`${
 				disabled && 'hover:cursor-not-allowed opacity-50'
 			} focus:outline-blue-600 focus:outline-2 rounded font-copy text-sm placeholder-gray-800 px-2 py-3 h-8 w-full outline outline-[1px] outline-gray-400 bg-gray-100 ${
 				className || ''
 			}`}
+			{...props}
 		/>
 	);
 };
