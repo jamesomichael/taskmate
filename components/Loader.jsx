acfe2acdc7245e57dfc2e14a8cff2719478b2383
James Michael <jomichael1994@gmail.com>
2025-03-20 17:48:06 +0000

    allow the user to define loader size

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,7 +1,9 @@
-const Loader = () => {
+const Loader = ({ size = 10 }) => {
 	return (
 		<div className="flex items-center justify-center w-full h-full">
-			<div className="w-10 h-10 border-2 border-t-transparent border-blue-600 rounded-full animate-spin"></div>
+			<div
+				className={`w-${size} h-${size} aspect-square border-2 border-t-transparent border-blue-600 rounded-full animate-spin`}
+			></div>
 		</div>
 	);
 };
